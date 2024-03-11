e1285aeacd7a273bb4e6e0408ab075a29abdc0fe
Kevin <kevin.cj.lin@toastmasters.org.tw>
2024-03-11 16:46:53 +0800

    init

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <UCard>
+    <template #header>
+      識別證產生器
+    </template>
+    <UContainer class="flex">
+      <UInput type="file" v-model="value" />
+      <UButton @click="click">產生識別證</UButton>
+    </UContainer>
+  </UCard>
+</template>
+
+<script setup lang="ts">
+const value = ref('')
+const click = () => {
+  console.log(value.value)
+}
+</script>
